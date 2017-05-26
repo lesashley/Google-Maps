8d9e38ded28015f67119de1b51cde43dc3ce5249
lesahley <lesashley56@gmail.com>
2017-05-26 14:46:37 -0500

    Agregando ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046}
+  var laboratoriaLima = {lat: -12.0520763,lng: -77.0439461}
   var map = new google.maps.Map(document.getElementById("map"),{
     zoom:18,
     center: laboratoriaLima
@@ -27,9 +27,46 @@ function initMap() {
     map.setCenter({lat:latitud, lng:longitud});
   }
 
-  var funcionError = function (error) {
+  var funcionError = function(error) {
     alert("Tenemos un problema con encontrar tu ubicación");
   }
 
   document.getElementById("encuentrame").addEventListener("click",buscar);
+
+  var inputPartida = document.getElementById("partida");
+  var inputDestino = document.getElementById("destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var tarifa = document.getElementById("tarifa");
+  
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: "DRIVING"
+    },function (response, status){
+      if (status === "OK") {
+        var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+        tarifa.classList.remove("none");
+        var costo =  distancia * 1.75;
+        if (costo < 4) {
+          tarifa.innerHTML = "S/. 4";
+        }
+        tarifa.innerHTML = "S/. " + parseInt(costo);
+        directionsDisplay.setDirections(response);
+        // miUbicacion.setMap(null);
+      } else {
+        window.alert("No encontramos una ruta.");
+      }
+    });
+  }
+  directionsDisplay.setMap(map);
+  var trazarRuta = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById("trazar").addEventListener("click",trazarRuta);
 }
